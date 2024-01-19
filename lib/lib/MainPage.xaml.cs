90d8ae67fe940870209c84795abecde4ca829c2f
Sakuta1126 <maildosteam@gmail.com>
2024-01-19 19:54:25 +0100

    Dodano logike strony glownej (okres 1/2)

--- lib/lib/MainPage.xaml.cs
+++ lib/lib/MainPage.xaml.cs
@@ -21,5 +21,77 @@ namespace lib
 
         }
 
+        public async void dodaj()
+        {
+            //User x = new User()
+            //{
+            //    Name = "Marcin",
+            //    Surname = "Gawron",
+            //    Login = "000001n",
+            //    Password = "admin123",
+            //    IsTeacher = true
+            //};
+            //await App.Database.InsertUser(x);
+            //Subject sbj = new Subject()
+            //{
+            //    Name = "Programowanie"
+            //};
+            //await App.Database.InsertSubject(sbj);
+            //Score s = new Score()
+            //{
+            //    User_id = 1,
+            //    Subject_id = 1,
+            //    Subject_name = "Programowanie",
+            //    Value = "5+",
+            //    Date = DateTime.Now,
+            //    Description = "Sprawdzian",
+            //    Period = "Okres 1"
+            //};
+            //await App.Database.InsertScore(s);
+        }
+
+        public async void UploadData()
+        {
+            Wyniki.ItemsSource = await App.Bazadanych.PobierzWyniki();
+
+            List<List<string>> semestr1 = new List<List<string>>();
+            List<List<string>> semestr2 = new List<List<string>>();
+
+            var przedmioty = await App.Bazadanych.PobierzPrzedmioty();
+            foreach (var przedmiot in przedmioty)
+            {
+                List<string> row = new List<string>();
+
+                var scoriesPeriodOne = await App.Bazadanych.PobierzWynik(this.uczen.Uczen_id, przedmiot.Przedmiot_id, "Okres 1");
+                string scoriesPeriodOneText = "";
+                foreach (var score in scoriesPeriodOne)
+                {
+                    scoriesPeriodOneText += score.wartosc + " ";
+                }
+                row.Add(scoriesPeriodOneText);
+                row.Add(przedmiot.Przedmiot_nazwa);
+
+                semestr1.Add(row);
+            }
+
+            foreach (var przedmiot in przedmioty)
+            {
+                List<string> row = new List<string>();
+
+                var scoriesPeriodTwo = await App.Bazadanych.PobierzWynik(this.uczen.Uczen_id, przedmiot.Przedmiot_id, "Okres 2");
+                string scoriesPeriodTwoText = "";
+                foreach (var score in scoriesPeriodTwo)
+                {
+                    scoriesPeriodTwoText += score.wartosc + " ";
+                }
+                row.Add(scoriesPeriodTwoText);
+                row.Add(przedmiot.Przedmiot_nazwa);
+
+                semestr1.Add(row);
+            }
+
+            Semestr1.ItemsSource = semestr1;
+            Semestr2.ItemsSource = semestr2;
+        }
     }
 }
--- lib/lib/Przedmiot.cs
+++ lib/lib/Przedmiot.cs
@@ -11,7 +11,7 @@ namespace lib
         public int Wynik_id { get; set; }
         public int Uczen_id { get; set; }
         public int Przedmiot_id { get; set; }
-        public string Przedmiot_name { get; set; }
+        public string Przedmiot_nazwa { get; set; }
         public string Wartosc { get; set; }
         public DateTime Data { get; set; }
         public string Opis { get; set; }
